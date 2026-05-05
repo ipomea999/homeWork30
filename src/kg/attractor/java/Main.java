2797dc231e06e35b05f958f736deb14fecfeb8cb
Kutman <ipomea875@gmail.com>
2026-05-05 17:30:08 +0600

    Added test outputs in Main to check stream homework methods

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,16 +1,35 @@
 package kg.attractor.java;
 
+import kg.attractor.java.homework.RestaurantOrders;
+
 public class Main {
     public static void main(String[] args) {
-
 
         // это для домашки
         // выберите любое количество заказов, какое вам нравится.
 
-        //var orders = RestaurantOrders.read("orders_100.json").getOrders();
+        var orders = RestaurantOrders.read("orders_100.json");
         //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
         //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
 
         // протестировать ваши методы вы можете как раз в этом файле (или в любом другом, в котором вам будет удобно)
+
+        System.out.println("--- All Orders ---");
+        orders.printOrders(orders.getOrders());
+
+        System.out.println("\n--- Top 3 Orders ---");
+        orders.printOrders(orders.getTopOrders(3));
+
+        System.out.println("\n--- Lowest 3 Orders ---");
+        orders.printOrders(orders.getLowestOrders(3));
+
+        System.out.println("\n--- Most profitable home delivery ---");
+        System.out.println(orders.getMostProfitableHomeDelivery().getTotal());
+
+        System.out.println("\n--- Total cost of all orders ---");
+        System.out.println(orders.getTotalCostOfAllOrders());
+
+        System.out.println("\n--- Sorted unique emails ---");
+        orders.getUniqueSortedEmails().forEach(System.out::println);
     }
 }
